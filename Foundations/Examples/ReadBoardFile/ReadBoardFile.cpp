f2d7d2ed56429e7c4624672c5dfb817333030fc7
hj0rzh <ahmed.elzenary@aptiv.com>
2020-07-08 15:02:52 +0200

    add comments for readBardFile ex

--- Foundations/Examples/ReadBoardFile/ReadBoardFile.cpp
+++ Foundations/Examples/ReadBoardFile/ReadBoardFile.cpp
@@ -13,10 +13,10 @@ void ReadBoardFile(string filePath)
   string line;
   ifstream f1;
   f1.open(filePath);
-  if(f1.is_open()==true)
+  if(f1)/*will be evaluated as false bool in case of failed filure*/
   {
-    while(getline(f1,line))
-   cout<<line<<"\n"; 
+    while(getline(f1,line))/*this return object from fstream but during conditions it is evaluated as bool and will be false incase of EOF*/  
+    { cout<<line<<"\n"; }
   }
   
   
