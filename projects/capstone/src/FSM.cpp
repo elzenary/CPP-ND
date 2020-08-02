c915a90ddbea88065dfb7ee90199cd447bbaef55
Ahmed Raafat Elzenary <elzenary@gmail.com>
2020-08-02 12:09:45 +0200

    update finite state manager implementation

--- projects/capstone/src/FSM.cpp
+++ projects/capstone/src/FSM.cpp
@@ -1,14 +1,23 @@
+#include "sha256.h"
+#include"UserDB.h"
+#include"UserRegister.h"
+#include"UserLogin.h"
+#include"bankOperations.h"
 #include"FSM.h"
 #include<iostream>
 using namespace std;
+
+/*global var which holds the currenf state*/
 windowStates_t windowStates=initial_w;
 
-/*helper function which change the state based on user input*/
+/*helper function which change the state based on user input
+this function is called in different states to hadle the program traversing*/
 void traverseOptions()
 {
    cout<<"traversing options"<<endl;
    cout<<"enter l for login"<<endl;
    cout<<"enter r for register"<<endl;
+   cout<<"enter i for initial window"<<endl;
    /*TODO move to login or register window*/
     char userInput;
     bool validInput=true;
@@ -38,20 +47,23 @@ void traverseOptions()
    }while(validInput);
 }
 
+/*initial window after launching the program
+the initial state handling*/
 void initialWindow()
 {
    cout<<"welcome to Bank inital window"<<endl;
    traverseOptions();
-
-   
 }
+
+/*this function represent user login state
+and handles login operations*/
 void loginWindow()
 {
     string usr,pass;
    cout<<"welcome to Bank login window"<<endl;
    cout<<"options"<<endl;
-   cout<<"enter user name then space then password"<<endl;
-   cout<<"Example Ahmed pass123"<<endl;
+   cout<<"enter userName space password"<<endl;
+   cout<<"Example: Ahmed pass123"<<endl;
    cin>>usr>>pass;
    /*TODO verify login*/
    /*if login fails*/
@@ -61,6 +73,9 @@ void loginWindow()
    /*if login pass*/
    /*TODO move to user window*/
 }
+
+/*this function represet user register state
+and handles register operations*/
 void registerWindow()
 {
     string usr,pass;
@@ -68,7 +83,7 @@ void registerWindow()
     char action;
    cout<<"welcome to Bank register window"<<endl;
    cout<<"options"<<endl;
-   cout<<"enter user name then space then password then space initial balance"<<endl;
+   cout<<"enter userName  password initialBalance"<<endl;
    cout<<"Example Ahmed pass123 5000"<<endl;
    cin>>usr>>pass>>balance;
    /*TODO verify register and show register output*/
@@ -79,21 +94,51 @@ void registerWindow()
    }
 }
 
-{
+/*this function should handle user operations after login
+deposit withdraw getBalance*/
 void userWindow()
 {
+   char action;
    cout<<"welcome user window"<<endl;
    cout<<"options"<<endl;
    cout<<"enter d for deposit"<<endl;
    cout<<"enter w for withdraw"<<endl;
    cout<<"enter g for get balance"<<endl;
+    char userInput;
+    bool validInput=true;
+   do
+       {
+       cin>>userInput;
+       /*implement transition conditions*/
+       switch(userInput)
+       {
+           case 'd':
+           case 'D':
+            /*get amount from the user*/
+            /*do operation*/
+            /*show result*/
+            break;
+           case 'w':
+           case 'W':
+            /*get amount from the user*/
+            /*do operation*/
+            /*show result*/
+            break;      
+           case 'g':
+           case 'G':
+            /*do operation*/
+            /*show result*/
+            break;     
+          default:
+            cout<<"please enter valid input"<<endl;
+            validInput=false;
+            break;
+       }
+   }while(validInput);
     
 }
 void FSM_Manger()
 {
-    
-// enum class windowStates_t={initial_w,login_w,register_w,user_w,admin_W};
-    /*TODO handling the movement between different states*/
     switch(windowStates)
     {
         case windowStates_t::initial_w:
@@ -112,12 +157,10 @@ void FSM_Manger()
             userWindow();
         
         break;
-        case windowStates_t::admin_W  :
-            adminWindow()
-        
-        break;
         default:
-        
+            /*error*/
+            /*show an error occured error*/
+            /*go to initial window*/
         break;
         
         break;        
--- projects/capstone/src/FSM.h
+++ projects/capstone/src/FSM.h
@@ -1,6 +1,6 @@
 #pragma once
 
-enum class windowStates_t={initial_w,login_w,register_w,user_w,admin_W};
+enum class windowStates_t={initial_w,login_w,register_w,user_w};
 void initialWindow();
 void loginWindow();
 void registerWindow();
